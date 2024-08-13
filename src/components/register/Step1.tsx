663d535097a28e1ffd976edd29c4fff9a5d0dac1
Junseo Kim <semiwest2@gmail.com>
2024-08-14 07:09:30 +0900

    #8 숙소등록 1단계 완성

--- src/components/register/Step1.tsx
+++ src/components/register/Step1.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { StepProps } from '../../app/types/types';
 
 const Step1: React.FC<StepProps> = ({ setFormData, formData, nextStep }) => {
-  const [maxOccupancy, setMaxOccupancy] = useState<number>(formData.maxOccupancy ? Number(formData.maxOccupancy) : 0);
+  const [maxOccupancy, setMaxOccupancy] = useState<number>(formData.maxOccupancy ? Number(formData.maxOccupancy) : 1);
   const [bedCount, setBedCount] = useState<number>(formData.bedCount || 1);
   const [phone, setPhone] = useState<string>(formData.phone || '');
 
@@ -18,36 +18,75 @@ const Step1: React.FC<StepProps> = ({ setFormData, formData, nextStep }) => {
     }
   };
 
+  const handleIncrement = () => {
+    setMaxOccupancy(maxOccupancy + 1);
+  };
+
+  const handleDecrement = () => {
+    if (maxOccupancy > 1) {
+      setMaxOccupancy(maxOccupancy - 1);
+    }
+  };
+
   return (
     <div className="p-6 flex flex-col items-center">
-      <h2 className="text-xl font-bold mb-6 text-center">숙소의 기본정보를 입력해주세요.</h2>
-
-      <div className="w-full mb-4">
-        <label className="block text-sm text-gray-700 mb-2 text-center">숙박 가능한 인원은 몇 명인가요?</label>
-        <div className="flex justify-center">
-          <input
-            type="number"
-            value={maxOccupancy}
-            onChange={(e) => setMaxOccupancy(Number(e.target.value))}
-            className="text-center p-2 border border-gray-300 rounded w-1/2"
-          />
+      <h2 className="text-4xl font-bold mb-6 text-center">숙소의 기본정보를 입력해주세요</h2>
+      <br />
+      <br />
+      <div className="w-3/4 mb-4">
+        <label className="block text-xl font-bold mb-2 text-center">
+          <span className="inline-block rounded-md bg-orange-300 bg-opacity-50 px-2">
+            숙박 가능한 인원 수를 입력해주세요
+          </span>
+        </label>
+        <div className="flex justify-between items-center bg-gray-100 p-2 rounded w-full max-w-sm mx-auto">
+          <span className="text-xl font-bold">최대 인원</span>
+          <div className="flex items-center">
+            <button onClick={handleDecrement} className="p-2">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+            </button>
+            <span className="text-xl mx-4">{maxOccupancy === Infinity ? '∞' : maxOccupancy}</span>
+            <button onClick={handleIncrement} className="p-2">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
-
-      <div className="w-full mb-4">
-        <label className="block text-sm text-gray-700 mb-2 text-center">구비되어 있는 침구는 얼마나 되나요?</label>
-        <div className="flex justify-center">
-          <input
-            type="number"
-            value={bedCount}
-            onChange={(e) => setBedCount(Number(e.target.value))}
-            className="text-center p-2 border border-gray-300 rounded w-1/2"
-          />
+      <br />
+      <div className="w-3/4 mb-4">
+        <label className="block text-xl font-bold mb-2 text-center">
+          <span className="inline-block rounded-md bg-orange-300 bg-opacity-50 px-2">
+            구비되어 있는 침구 수를 입력해주세요
+          </span>
+        </label>
+        <div className="flex justify-between items-center bg-gray-100 p-2 rounded w-full max-w-sm mx-auto">
+          <span className="text-xl font-bold">침구</span>
+          <div className="flex items-center">
+            <button onClick={() => setBedCount(bedCount > 0 ? bedCount - 1 : 0)} className="p-2">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+            </button>
+            <span className="text-xl mx-4">{bedCount}</span>
+            <button onClick={() => setBedCount(bedCount + 1)} className="p-2">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
-
-      <div className="w-full mb-6">
-        <label className="block text-sm text-gray-700 mb-2 text-center">운영자분의 전화번호를 알려주세요!</label>
+      <br />
+      <div className="w-3/4 mb-6">
+        <label className="block text-xl font-bold mb-2 text-center">
+          <span className="inline-block rounded-md bg-orange-300 bg-opacity-50 px-2">
+            운영자분의 전화번호를 입력해주세요
+          </span>
+        </label>
         <div className="flex justify-center">
           <input
             type="tel"
@@ -57,8 +96,8 @@ const Step1: React.FC<StepProps> = ({ setFormData, formData, nextStep }) => {
           />
         </div>
       </div>
-
-      <button onClick={handleNext} className="bg-orange-500 text-white p-3 rounded-full w-full max-w-xs">
+      <br />
+      <button onClick={handleNext} className="bg-customColor text-black font-bold p-3 rounded-lg w-1/2">
         다음
       </button>
     </div>
--- src/components/register/Step2.tsx
+++ src/components/register/Step2.tsx
@@ -1,43 +1,63 @@
 import React, { useState } from 'react';
 import { StepProps } from '../../app/types/types';
 
-  const Step2: React.FC<StepProps> = ({ setFormData, formData, nextStep, prevStep }) => {
-  const [location, setLocation] = useState(formData.location || '');
+const Step4: React.FC<StepProps> = ({ setFormData, formData, prevStep, submitForm }) => {
+  const [description, setDescription] = useState<string>(formData.description || '');
+  const [photos, setPhotos] = useState<FileList | null>(null);
 
-  const handleNext = () => {
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setPhotos(e.target.files);
+    }
+  };
+
+  const handleSubmit = () => {
+    // Convert FileList to an array if needed for further processing
+    const photosArray = photos ? Array.from(photos) : null;
+
     setFormData({
       ...formData,
-      location
+      description,
+      photos,
     });
-    if (nextStep) {
-        nextStep();
-      }
+    if (submitForm) {
+      submitForm();
+    }
   };
 
   return (
     <div className="p-6">
-      <h2 className="text-2xl font-bold mb-4">숙소의 위치를 입력해주세요!</h2>
-
+      <h2 className="text-4xl font-black mb-6 text-center">숙소의 기본정보를 이어서 입력해주세요</h2>
       <div className="mb-4">
-        <label className="block text-gray-700">주소를 입력하세요.</label>
+        <label className="block text-gray-700">숙소를 잘 나타내는 사진을 등록해주세요.</label>
         <input
-          type="text"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
+          type="file"
+          multiple
+          onChange={handleFileChange}
           className="mt-2 p-2 border border-gray-300 rounded w-full"
         />
+      </div>
+
+      <div className="mb-4">
+        <label className="block text-gray-700">숙소를 간단하게 소개해주세요.</label>
+        <textarea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          className="mt-2 p-2 border border-gray-300 rounded w-full"
+          placeholder="어떤 내용이든 자유롭게 작성해주세요 :)"
+        ></textarea>
       </div>
 
       <div className="flex justify-between">
         <button onClick={prevStep} className="bg-gray-300 text-black p-2 rounded">
           이전
         </button>
-        <button onClick={handleNext} className="bg-orange-500 text-white p-2 rounded">
-          다음
+        <button onClick={handleSubmit} className="bg-orange-500 text-white p-2 rounded">
+          완료
         </button>
       </div>
     </div>
   );
 };
 
-export default Step2;
+export default Step4;
