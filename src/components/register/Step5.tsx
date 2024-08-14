cfd72d7151e760503395262c62289d919f4d4d87
jihyeon <wlgus7464@gmail.com>
2024-08-14 10:58:33 +0900

    숙소 등록 페이지
    메인으로 돌아가기 링크 추가

--- src/components/register/Step5.tsx
+++ src/components/register/Step5.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
+import {useRouter} from "next/navigation";
 
 const Step5 = () => {
-
+    const router = useRouter()
 
   return (
     <div className="p-6 flex flex-col items-center">
         <h2 className="text-4xl font-black mb-12 text-center">숙소 등록이 완료되었습니다!!</h2>
 
-        <button className="bg-customColor text-black font-bold p-3 rounded-lg w-1/2">
+        <button className="bg-customColor text-black font-bold p-3 rounded-lg w-1/2"
+                onClick={() =>router.push('/')}>
             메인으로 돌아가기
         </button>
     </div>
